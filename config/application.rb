9cfae9119e599e4693e4dd39b8a9348aac11c31e
qbylucky <18600211621@163.com>
2017-03-13 00:05:07 +0800

    Rails5 config.eager_load_paths

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,12 @@ module RailsShop
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.autoload_paths += %W[#{Rails.root}/lib]
+    # 线程不安全
+    # config.enable_dependency_loading = true
+    # config.autoload_paths += %W[#{Rails.root}/lib]
+    # config.autoload_paths << Rails.root.join('lib')
+
+    config.eager_load_paths << Rails.root.join('lib')
 
     config.generators do |generator|
       generator.assets false
